5fdb22bd5a8c0afec9bf376e36da8bfe40046a5b
Nata Hreshchyshyn <natalihreshchyshyn@gmail.com>
2021-08-11 05:11:33 +0300

    Adding HW Setup project and ORM Practice

---
+++ homeworks/car_dealer/car_dealer/car_showroom/models/dealer/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Dealer(User):
+
+    title = models.CharField(max_length=255)
+    city = models.ForeignKey(
+        'car_showroom.City',
+        on_delete=models.CASCADE,
+        related_name='dealers'
+    )
+
+    class Meta:
+        app_label = 'car_showroom'
+        verbose_name = 'Dealer'
+        verbose_name_plural = 'Dealers'
+
+    def __str__(self):
+        return f'Dealer: {self.first_name} {self.last_name} from {self.city}'
+
+
+class City(models.Model):
+    name = models.CharField(max_length=75)
+    country = models.ForeignKey(
+        'car_showroom.Country',
+        on_delete=models.CASCADE,
+        related_name='cities',
+        null=True,
+    )
+
+    class Meta:
+        app_label = 'car_showroom'
+        verbose_name = 'City'
+        verbose_name_plural = 'Cites'
+
+    def __str__(self):
+        return f'{self.name} city'
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=75)
+    code = models.PositiveIntegerField()
+
+    class Meta:
+        app_label = 'car_showroom'
+        verbose_name = 'Country'
+        verbose_name_plural = 'Countries'
+
+    def __str__(self):
+        return f'Country {self.name}'
